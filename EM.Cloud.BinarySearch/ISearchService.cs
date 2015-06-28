bb47503273ea830c7e77afa3b1b3dffbd6d54c21
Eugene-Murray <eugene.murray@gmail.com>
2015-06-28 21:37:06 +0100

    add solution

---
+++ EM.Cloud.BinarySearch/ISearchService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EM.Cloud.BinarySearch
+{
+    public interface ISearchService
+    {
+        int FindWinner(int[] inputArray);
+
+        object BinarySearchIterative(int[] inputArray, int? searchKey, int min, int max);
+
+        object BinarySearchRecursive(int[] inputArray, int? searchKey, int min, int max);
+    }
+}
